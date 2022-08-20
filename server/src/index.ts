3f1ee82076916109f9429c98664d249831522a1a
isaiahiu <isaiah.iu@hotmail.com>
2022-08-20 17:38:02 -0400

    Hardcode users list for testing purposes, then create GraphQL schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,33 @@
 import { buildSchema } from "graphql";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+
+const users = [
+	{ id: 1, name: "John Doe", email: "johndoe@gmail.com" },
+	{ id: 2, name: "Jane Doe", email: "janedoe@gmail.com" },
+	{ id: 3, name: "Mike Doe", email: "mikedoe@gmail.com" },
+];
+
+const schema = buildSchema(`
+    input UserInput {
+        email: String!
+        name: String!
+
+    }
+
+    type User {
+        id: Int!
+        name: String!
+        email: String!
+    }
+
+    type Mutation {
+        createUser(input: UserInput): User
+        updateUser(id: Int!, input: UserInput): User
+    }
+
+    type Query {
+        getUser(id: String): User
+        getUsers: [User]
+    }
+`);
