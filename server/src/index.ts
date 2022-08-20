cf63674b6e703503975400e6064451496a343ab0
isaiahiu <isaiah.iu@hotmail.com>
2022-08-20 17:40:29 -0400

    Define resolvers for schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,3 +39,34 @@ type User = {
 };
 
 type UserInput = Pick<User, "email" | "name">;
+
+const getUser = (args: { id: number }): User | undefined =>
+	users.find(u => u.id === args.id);
+
+const getUsers = (): User[] => users;
+
+const createUser = (args: { input: UserInput }): User => {
+	const user = {
+		id: users.length + 1,
+		...args.input,
+	};
+	users.push(user);
+
+	return user;
+};
+
+const updateUser = (args: { user: User }): User => {
+	const index = users.findIndex(u => u.id === args.user.id);
+	const targetUser = users[index];
+
+	if (targetUser) users[index] = args.user;
+
+	return targetUser;
+};
+
+const root = {
+	getUser,
+	getUsers,
+	createUser,
+	updateUser,
+};
