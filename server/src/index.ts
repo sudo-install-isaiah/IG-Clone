39c205644561ec273bf013dd5852d0c9c4c3de52
isaiahiu <isaiah.iu@hotmail.com>
2022-08-20 21:03:47 -0400

    Changes to use TypeGraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,89 +1,29 @@
-import { buildSchema } from "graphql";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 
-const users = [
-	{ id: 1, name: "John Doe", email: "johndoe@gmail.com" },
-	{ id: 2, name: "Jane Doe", email: "janedoe@gmail.com" },
-	{ id: 3, name: "Mike Doe", email: "mikedoe@gmail.com" },
-];
+import { UserResolver } from "./users/users.resolvers";
 
-const schema = buildSchema(`
-    input UserInput {
-        email: String!
-        name: String!
+async function main() {
+	const schema = await buildSchema({
+		resolvers: [UserResolver],
+		emitSchemaFile: true,
+	});
 
-    }
+	const app = express();
 
-    type User {
-        id: Int!
-        name: String!
-        email: String!
-    }
+	app.use(
+		"/graphql",
+		graphqlHTTP({
+			schema: schema,
+			graphiql: true,
+		})
+	);
 
-    type Mutation {
-        createUser(input: UserInput): User
-        updateUser(id: Int!, input: UserInput): User
-    }
+	app.listen(8000);
 
-    type Query {
-        getUser(id: String): User
-        getUsers: [User]
-    }
-`);
+	console.log("Running a GraphQL API server at http://localhost:8000/graphql");
+}
 
-type User = {
-	id: number;
-	name: string;
-	email: string;
-};
-
-type UserInput = Pick<User, "email" | "name">;
-
-const getUser = (args: { id: number }): User | undefined =>
-	users.find(u => u.id === args.id);
-
-const getUsers = (): User[] => users;
-
-const createUser = (args: { input: UserInput }): User => {
-	const user = {
-		id: users.length + 1,
-		...args.input,
-	};
-	users.push(user);
-
-	return user;
-};
-
-const updateUser = (args: { user: User }): User => {
-	const index = users.findIndex(u => u.id === args.user.id);
-	const targetUser = users[index];
-
-	if (targetUser) users[index] = args.user;
-
-	return targetUser;
-};
-
-const root = {
-	getUser,
-	getUsers,
-	createUser,
-	updateUser,
-};
-
-const app = express();
-
-app.use(
-	"/graphql",
-	graphqlHTTP({
-		schema: schema,
-		rootValue: root,
-		graphiql: true,
-	})
-);
-
-const PORT = 8000;
-
-app.listen(PORT);
-
-console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
+main();
